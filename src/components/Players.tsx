8098de7cc30d79882459dea7f38988da6937c1ab
CJ Quines <cj@cjquines.com>
2021-07-01 15:08:02 -0400

    add asking button

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,14 +1,50 @@
 import React from "react";
 
+import { Card, genDeck } from "lib/cards";
+import { CFish as C } from "lib/cfish";
 import { Client } from "lib/client";
+
+export namespace CardSelector {
+  export type Props = {
+    callback: (Card) => void;
+    close: () => void;
+  };
+}
+
+export class CardSelector extends React.Component<CardSelector.Props> {
+  render() {
+    return (
+      <div className="cardSelector">
+        {[...genDeck()].map((card) => (
+          <button onClick={(e) => this.props.callback(card)}>
+            {card.toString()}
+          </button>
+        ))}
+        <button onClick={(e) => this.props.close()}>cancel</button>
+      </div>
+    );
+  }
+}
 
 export namespace Players {
   export type Props = {
     client: Client;
   };
+
+  export type State = {
+    askee: number | null;
+  };
 }
 
-export class Players extends React.Component<Players.Props> {
+export class Players extends React.Component<Players.Props, Players.State> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      askee: null,
+    };
+  }
+
   render() {
     const { client } = this.props;
     const { engine, users } = client;
@@ -30,6 +66,22 @@ export class Players extends React.Component<Players.Props> {
               ) : engine.userOf[seat] !== null ? null : (
                 <button onClick={(e) => client.seatAt(seat)}>sit down</button>
               )}
+              {engine.phase === C.Phase.ASK &&
+              engine.asker === engine.ownSeat &&
+              engine.teamOf(seat) !== engine.teamOf(engine.ownSeat) ? (
+                <button onClick={(e) => this.setState({ askee: seat })}>
+                  ask
+                </button>
+              ) : null}
+              {this.state.askee === seat ? (
+                <CardSelector
+                  callback={(card) => {
+                    client.ask(seat, card);
+                    this.setState({ askee: null });
+                  }}
+                  close={() => this.setState({ askee: null })}
+                />
+              ) : null}
             </div>
           </div>
         ))}
